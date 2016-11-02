c616395e2b80a393c74f937f14cd8916ca9cc6ee
Wayne <wayne.huang@kdanmobile.com>
2016-11-02 09:59:59 +0800

    Add some examples 0.0

--- app/src/test/java/com/dowob/rxandroidexample/MainActivityTest.java
+++ app/src/test/java/com/dowob/rxandroidexample/MainActivityTest.java
@@ -46,4 +46,68 @@ public class MainActivityTest {
         s.unsubscribe();
     }
 
+    // 不使用 blocking 在非主 thread subscribe
+    @Test
+    public void test2() throws Exception {
+        println("test2 start");
+        Observable.just(1, 2, 3)
+                .map(n -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    println("map");
+                    return n;
+                })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .subscribe(n -> {
+                   println("sub " + n);
+                });
+        println("test2 end");
+    }
+
+    // 使用 blocking 在非主 thread subscribe
+    @Test
+    public void test3() throws Exception {
+        println("test3 start");
+        Observable.just(1, 2, 3)
+                .map(n -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    println("map");
+                    return n;
+                })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .toBlocking()
+                .forEach(n -> {
+                    println("for each " + n);
+                });
+        println("test3 end");
+    }
+
+    // 直接拿值
+    @Test
+    public void test4() {
+        println("test4 start");
+        int num = Observable.just(1)
+                .map(n -> {
+                    println("map");
+                    return n + 399;
+                })
+                .toBlocking()
+                .first();
+        println("num = " + num);
+        println("test4 end");
+    }
+
+    private void println(String s) {
+        long id = Thread.currentThread().getId();
+        System.out.println(String.format("[Thread %3d] : %s", id, s));
+    }
 }
